3739fe3c6b2e62376b9ba07ed66d35807307c0d3
cedric <c.lebeau69@gmail.com>
2024-07-02 21:38:54 +0200

    Init project

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {RadixDappToolkit, RadixNetwork} from "@radixdlt/radix-dapp-toolkit";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+}
